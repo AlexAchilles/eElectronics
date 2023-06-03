e6d488efa88bd3c8224f94d56519811e4c02c799
Alexandre Achilles <119471172+AlexAchilles@users.noreply.github.com>
2023-06-03 18:09:23 -0300

    Update home.php

--- themes/web/home.php
+++ themes/web/home.php
@@ -2,33 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>eElectronics - HTML eCommerce Template</title>
-    
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="<?=url("assets/web/css/owl.carousel.css"); ?>">
-    <link rel="stylesheet" href="<?=url("assets/web/css/style.css"); ?>">
-    <link rel="stylesheet" href="<?=url("asstes/web/css/responsive.css"); ?>">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
    
@@ -65,10 +39,10 @@
                                     <div class="row">
                                         <div class="col-md-6 col-md-offset-6">
                                             <div class="slide-content">
-                                                <h2>We are awesome</h2>
+                                                <h2>Nós somos incríveis</h2>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ab molestiae minus reiciendis! Pariatur ab rerum, sapiente ex nostrum laudantium.</p>
-                                                <a href="" class="readmore">Learn more</a>
+                                                <a href="" class="readmore">Saiba mais</a>
                                             </div>
                                         </div>
                                     </div>
@@ -86,9 +60,9 @@
                                     <div class="row">
                                         <div class="col-md-6 col-md-offset-6">
                                             <div class="slide-content">
-                                                <h2>We are great</h2>
+                                                <h2>Nós somos grandes</h2>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aspernatur, dolorum harum molestias tempora deserunt voluptas possimus quos eveniet, vitae voluptatem accusantium atque deleniti inventore. Enim quam placeat expedita! Quibusdam!</p>
-                                                <a href="" class="readmore">Learn more</a>
+                                                <a href="" class="readmore">Saiba mais</a>
                                             </div>
                                         </div>
                                     </div>
@@ -106,10 +80,10 @@
                                     <div class="row">
                                         <div class="col-md-6 col-md-offset-6">
                                             <div class="slide-content">
-                                                <h2>We are superb</h2>
+                                                <h2>Nós somos supers</h2>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, eius?</p>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti voluptates necessitatibus dicta recusandae quae amet nobis sapiente explicabo voluptatibus rerum nihil quas saepe, tempore error odio quam obcaecati suscipit sequi.</p>
-                                                <a href="" class="readmore">Learn more</a>
+                                                <a href="" class="readmore">Saiba mais</a>
                                             </div>
                                         </div>
                                     </div>
@@ -130,32 +104,33 @@
                 <div class="col-md-3 col-sm-6">
                     <div class="single-promo">
                         <i class="fa fa-refresh"></i>
-                        <p>30 Days return</p>
+                        <p>Retorno em 30 dias</p>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <div class="single-promo">
                         <i class="fa fa-truck"></i>
-                        <p>Free shipping</p>
+                        <p>Entrega gratuita</p>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <div class="single-promo">
                         <i class="fa fa-lock"></i>
-                        <p>Secure payments</p>
+                        <p>Pagamentos seguros</p>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <div class="single-promo">
                         <i class="fa fa-gift"></i>
-                        <p>New products</p>
+                        <p>Produtos novos</p>
                     </div>
                 </div>
             </div>
         </div>
     </div> <!-- End promo area -->
     
-    <div class="maincontent-area">
+    <!-- Tenho que consertar isto depois -->
+    <!-- <div class="maincontent-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
             <div class="row">
@@ -257,9 +232,11 @@
                 </div>
             </div>
         </div>
-    </div> <!-- End main content area -->
-    
-    <div class="brands-area">
+    </div>  -->
+    <!-- End main content area -->
+
+    <!-- Tenho que consertar isto depois -->
+    <!-- <div class="brands-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
             <div class="row">
@@ -280,7 +257,8 @@
                 </div>
             </div>
         </div>
-    </div> <!-- End brands area -->
+    </div>  -->
+    <!-- End brands area -->
     
     <div class="product-widget-area">
         <div class="zigzag-bottom"></div>
@@ -288,11 +266,11 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="single-product-widget">
-                        <h2 class="product-wid-title">Top Sellers</h2>
-                        <a href="" class="wid-view-more">View All</a>
+                        <h2 class="product-wid-title">Mais Vendidos</h2>
+                        <a href="" class="wid-view-more">Ver todos</a>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-1.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Sony Smart TV - 2015</a></h2>
+                            <h2><a href="single-product.html">Playstation 4</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -301,12 +279,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$1400,99</ins> <del>R$2560,00</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-2.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Apple new mac book 2015</a></h2>
+                            <h2><a href="single-product.html">Samsung Galaxy Z Flip</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -315,12 +293,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$3700,99</ins> <del>R$4250,99</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-3.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Apple new i phone 6</a></h2>
+                            <h2><a href="single-product.html">Notebook Asus</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -329,18 +307,18 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$2300,00</ins> <del>R$3000,00</del>
                             </div>                            
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="single-product-widget">
-                        <h2 class="product-wid-title">Recently Viewed</h2>
-                        <a href="#" class="wid-view-more">View All</a>
+                        <h2 class="product-wid-title">Te Aguardando</h2>
+                        <a href="#" class="wid-view-more">Ver todos</a>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-4.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Sony playstation microsoft</a></h2>
+                            <h2><a href="single-product.html">LG - Smart TV OLED</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -349,12 +327,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$3400,00</ins> <del>R$4296,00</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-1.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Sony Smart Air Condtion</a></h2>
+                            <h2><a href="single-product.html">Playstation 4</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -363,12 +341,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$1400,99</ins> <del>R$2560,00</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-2.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Samsung gallaxy note 4</a></h2>
+                            <h2><a href="single-product.html">Samsung Galaxy Z Flip</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -377,18 +355,18 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$3700,99</ins> <del>R$4250,99</del>
                             </div>                            
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="single-product-widget">
-                        <h2 class="product-wid-title">Top New</h2>
-                        <a href="#" class="wid-view-more">View All</a>
+                        <h2 class="product-wid-title">Produtos Novos</h2>
+                        <a href="#" class="wid-view-more">Ver todos</a>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-3.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Apple new i phone 6</a></h2>
+                            <h2><a href="single-product.html">Notebook Asus</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -397,12 +375,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$2300,00</ins> <del>R$3000,00</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-4.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Samsung gallaxy note 4</a></h2>
+                            <h2><a href="single-product.html">LG - Smart TV OLED</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -411,12 +389,12 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$3400,00</ins> <del>R$4296,00</del>
                             </div>                            
                         </div>
                         <div class="single-wid-product">
                             <a href="single-product.html"><img src="assets/web/img/product-thumb-1.jpg" alt="" class="product-thumb"></a>
-                            <h2><a href="single-product.html">Sony playstation microsoft</a></h2>
+                            <h2><a href="single-product.html">Playstation 4</a></h2>
                             <div class="product-wid-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -425,7 +403,7 @@
                                 <i class="fa fa-star"></i>
                             </div>
                             <div class="product-wid-price">
-                                <ins>$400.00</ins> <del>$425.00</del>
+                                <ins>R$1400,99</ins> <del>R$2560,00</del>
                             </div>                            
                         </div>
                     </div>
